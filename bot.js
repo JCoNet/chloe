aa0eaf7faa30af3aaa89766df0fd845ac4e89658
JCoDog <jayconnolly14@icloud.com>
2018-09-23 11:32:51 +0100

    Update
    No longer a cancerous nightmare for servers

--- bot.js
+++ bot.js
@@ -42,8 +42,8 @@ bot.login(process.env.token);
 bot.on('ready', () => {
   console.log("Michelle Activated.");
    // bot.user.setActivity("the epic failz of Army_Killa44",{type: "STREAMING", url: "https://www.twitch.tv/army_killa44"});
-   // bot.user.setActivity("Developmental tunes w/MonsterCat", {type: "STREAMING", url: "https://www.twitch.tv/monstercat"});
-   bot.user.setActivity("YouTube", {type: "WATCHING"});
+   bot.user.setActivity("Developmental tunes w/MonsterCat", {type: "STREAMING", url: "https://www.twitch.tv/monstercat"});
+   // bot.user.setActivity("", {type: "WATCHING"});
 });
 
 bot.on('message', async message => {
@@ -57,44 +57,6 @@ bot.on('message', async message => {
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if (commandfile) commandfile.run(bot, message, args);
-
-  if (talkedRecently.has(message.author.id)) {
-    return;
-  } else {
-    // the user can type the command ... your command code goes here :)
-    if(message.content.toLowerCase().includes("stop")) {
-      talkedRecently.add(message.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(message.author.id);
-        }, 300000);
-      return message.channel.send(`Why should I <@${message.author.id}>? You on your period or something bitch?`);
-    };
-    if (message.content.toLowerCase().includes("i see")) {
-      talkedRecently.add(message.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(message.author.id);
-        }, 300000);
-      return message.channel.send(`We all see <@${message.author.id}>! Well except for blind twats like you that is.`);
-    };
-    if (message.content.toLowerCase().includes("oof")) {
-      talkedRecently.add(message.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(message.author.id);
-        }, 300000);
-      return message.channel.send(`Fucking hell <@${message.author.id}>! Do you know how much of a gay cunt you sound like by saying that?`);
-    };
-    if (message.author.id == "452716084294975507") {
-      talkedRecently.add(message.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(message.author.id);
-        }, 300000);
-      return message.channel.send("Well it appears we have a new law. The Great Fag Lord has just spoken.");
-    };
-  }; 
 
   // if (cmd == `${prefix}stats`) {
   //   message.channel.send(`I am currently being worked on by JCoDog for the release in september of my core version 1.0.0 (i am currently ${info.version})`);
