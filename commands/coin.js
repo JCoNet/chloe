a416f028597269f8eb995970ee1c7953858523a8
JCoDog <jayconnolly14@icloud.com>
2018-10-01 08:18:45 +0100

    Update
    Bug fix

--- commands/coin.js
+++ commands/coin.js
@@ -2,8 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-	message.catch().delete();
-	message.channel.reply("We are implimenting this feature currently... Please be patient with our developers.")
+	message.channel.send("We are implimenting this feature currently... Please be patient with our developers.")
 
 }
 
--- commands/spank.js
+++ commands/spank.js
@@ -3,13 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 	let id = message.mentions.users.first().id;
-	let recepient = bot.users.get(id);
 	let embed = new Discord.RichEmbed()
 		.setTitle("Though hath been spanked!")
-		.setDescrtiption(`You have just been spanked <@${recepeint}>`)
-		.addField("Spanked by", message.author.username)
-	message.catch().delete()
-	message.channel.send(embed)
+		.setDescrtiption(`You have just been spanked <@${id}>`)
+		.addField("Spanked by", message.author.username);
+	message.channel.send(embed);
 
 }
 
