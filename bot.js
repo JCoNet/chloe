509ac38ffa35bce90151010391588910c90f9cce
JCoDog <jayconnolly14@icloud.com>
2018-10-06 02:47:32 +0100

    Update
    Status

--- bot.js
+++ bot.js
@@ -40,8 +40,8 @@ bot.login(process.env.token);
 
 bot.on('ready', () => {
   console.log("Michelle Activated.");
-   // bot.user.setActivity("the epic failz of Army_Killa44",{type: "STREAMING", url: "https://www.twitch.tv/army_killa44"});
-   bot.user.setActivity("Developmental tunes w/MonsterCat", {type: "STREAMING", url: "https://www.twitch.tv/monstercat"});
+   bot.user.setActivity("the epic failz of Army_Killa44",{type: "STREAMING", url: "https://www.twitch.tv/army_killa44"});
+   // bot.user.setActivity("Developmental tunes w/MonsterCat", {type: "STREAMING", url: "https://www.twitch.tv/monstercat"});
    // bot.user.setActivity("", {type: "WATCHING"});
 });
 
