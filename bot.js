02f106a88bf0f9b018e6642f75d70b711c238feb
JCoDog <jayconnolly14@icloud.com>
2018-10-10 20:12:28 +0100

    Update
    Prefix check

--- bot.js
+++ bot.js
@@ -49,12 +49,11 @@ bot.on('ready', () => {
 bot.on('message', async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
-
   let prefix = config.prefix;
+  if (!message.content.startsWith(prefix)) return ;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if (commandfile) commandfile.run(bot, message, args);
 
