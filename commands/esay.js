658548ac4da99606d1c0764b41def59cddba26a4
JCoDog <jayconnolly14@icloud.com>
2018-10-10 18:41:47 +0100

    Update
    Shit happens

---
+++ commands/esay.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("OOF. You do not appear to have access to this command, you need to be able to manage messages to do this!");
+  let botmessage = args.join(" ");
+  message.delete().catch();
+  let embed = new Discord.RichEmbed()
+		.setTitle("AI Chat")
+		.setColor("#00ff00")
+		.addField("Message", botmessage);
+  message.channel.send(embed);
+}
+
+module.exports.help ={
+  name: "say"
+}
--- commands/mail.js
+++ commands/mail.js
@@ -4,17 +4,17 @@ module.exports.run = async (bot, message, args) => {
 	let sender = message.author.username;
 	let id = message.mentions.users.first().id;
 	let recepient = bot.users.get(id);
-  	let mail = args.join(" ").slice(22);
-  	message.delete().catch();
-  	let embed = new Discord.RichEmbed()
-  		.setTitle("Michelle Mail")
-  		.setDescription("The user messaging system through the bot.")
-  		.setColor("#00ff00")
-  		.addField("To", "You")
-  		.addField("From", sender)
-  		.addField("Message", mail)
-  		.setFooter("Reply by going to a server with this bot in and doing 'm/mail @user <message>'");
-  	recepient.send(embed);
+	let mail = args.join(" ").slice(22);
+	message.delete().catch();
+	let embed = new Discord.RichEmbed()
+		.setTitle("Michelle Mail")
+		.setDescription("The user messaging system through the bot.")
+		.setColor("#00ff00")
+		.addField("To", "You")
+		.addField("From", sender)
+		.addField("Message", mail)
+		.setFooter("Reply by going to a server with this bot in and doing 'm/mail @user <message>'");
+	recepient.send(embed);
 }
 
 module.exports.help ={
