46b6a334ac41a3e1b4ee94159aa51b3a7a5f7a91
JCoDog <jayconnolly14@icloud.com>
2018-10-10 19:07:30 +0100

    Update
    Patch of bug and added sleep.

--- commands/sleep.js
+++ commands/sleep.js
@@ -2,15 +2,10 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("OOF. You do not appear to have access to this command, you need to be able to manage messages to do this!");
-  let botmessage = args.join(" ");
   message.delete().catch();
-  let embed = new Discord.RichEmbed()
-		.setTitle("AI Chat")
-		.setColor("#00ff00")
-		.addField("AI Message", botmessage);
-  message.channel.send(embed);
+  message.channel.send(`Shutting down to 10% processing power, goodnight ${message.guild.name}.`);
 }
 
 module.exports.help ={
-  name: "esay"
+  name: "sleep"
 }
