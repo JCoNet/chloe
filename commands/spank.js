643582c6aa9d2d6d417bee501b492dbe4f759569
JCoDog <jayconnolly14@icloud.com>
2018-10-12 23:21:50 +0100

    Update commands/spank.js

--- commands/spank.js
+++ commands/spank.js
@@ -4,9 +4,14 @@ module.exports.run = async (bot, message, args) => {
 
 	// if (message.author.id != "415067295715557376" || message.author.id != "481872616508882944") return message.reply("You idiot, that command is for legends only!");
 
-	if (!message.member.id.some(x => ["415067295715557376", "481872616508882944"].includes(x.id))) return message.reply("You idiot, that command is for legends only!");
+	// if (!message.member.id.some(x => ["415067295715557376", "481872616508882944"].includes(x.id))) return message.reply("You idiot, that command is for legends only!");
+	
+	let ids = ["415067295715557376","481872616508882944"];
+	if (!ids.includes(message.author.id));
+
 	let recipient = message.mentions.members.first().id;
-	let sender = message.author.id;
+	let sender = message.author.idreturn message.reply("You idiot, that command is for legends only!");
+
 	message.delete().catch();
 	let embed = new Discord.RichEmbed()
 		.setTitle("Spankings")
