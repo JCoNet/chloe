b9e2a4f9aa0c274a321ec38245a01ffa35ea63ed
JCoDog <jayconnolly14@icloud.com>
2018-11-23 13:36:48 +0000

    Update
    Changes...

--- commands/stats.js
+++ commands/stats.js
@@ -1,15 +1,17 @@
 const Discord = require("discord.js");
+const stats = require("../package.json");
 
 module.exports.run = async (bot, message, args) => {
   let bicon = bot.user.displayAvatarURL;
   let statsembed = new Discord.RichEmbed()
     .setTitle("Bot Statistics")
-    .setDescription("The general information of the bot")
+    .setDescription(stats.description)
     .setColor("#33fede")
     .setThumbnail(bicon)
     .addField("Name", bot.user.username)
     .addField("Created on", bot.user.createdAt)
-    .addField("Current version", "2.0.0");
+    .addField("Current version", stats.version)
+    .setFooter(`Lead developer: ${stats.author}`);
   message.channel.send(statsembed);
 }
 
