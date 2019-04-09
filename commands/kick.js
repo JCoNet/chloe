37c806c61fb180efcfa488efeb4ef11ab01e8ac6
JCoDog <jayconnolly14@icloud.com>
2019-04-09 18:01:40 +0100

    Update 2.0.0
    Part one
    edited stats.js added Honey link
    added kick.js incomplete

---
+++ commands/kick.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!kUser) return message.reply("The specified user could not be found.");
+    let kReason = args.join(" ").slice(22);
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You have not got the right permissions.");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.reply("That user cannot be kicked.");
+
+    let kickEmbed = new Discord.RichEmbed()
+        .setTitle("Kick")
+        .setDescription("The following kick took place")
+        .setColor("#e68a00")
+        .addField("Kicked User", `${kUser} with the id ${kUser.id}`)
+        .addField("Time", message.createdAt)
+        .addField("Reason", kReason);
+    message.reply(kickEmbed)
+}
+
+module.exports.help = {
+  name: "kick"
+}
