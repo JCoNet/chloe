1788de1c683a430548112afad06cb7399a20d6ac
JCoDog <jayconnolly14@icloud.com>
2020-02-24 16:32:32 +0000

    Patching

--- commands/mood.js
+++ commands/mood.js
@@ -7,8 +7,8 @@ module.exports.run = async (bot, message, args, useprefix) => {
     let moodEmbed = new Discord.RichEmbed()
         .setTitle("Chloe Support")
         .setColor("#f03090")
-        .setDescription("Here is a song to help you lift your mood. If you need to chat, message JCoDog.")
-        .addField("Link to song", '[Click here to open the song on spotify!](https://open.spotify.com/album/5AEgsSGBHblhs5ZRFOLI4n?si=skWigbeiRd6Ud1HKG0uJJA "A song that helped JCoDog when he was down, hopefully it will help you too.")');
+        .setDescription("Here is a playlist to help you lift your mood. If you need to chat, message JCoDog.")
+        .addField("Link to playlist", '[Click here to open the playlist on spotify!](https://open.spotify.com/playlist/7y6R3Nr4ZYqVeFIPHjmP9i?si=g267nR7wRG2iCR6PS99m1g"A playlist that helped JCoDog when he was down, hopefully it will help you too.")');
     message.reply(moodEmbed).catch(err => console.log(err));
 
 };
