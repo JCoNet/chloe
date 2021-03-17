8c08a1985327fd10747bba7f29d65d197f9e8311
JCoDog <jayconnolly14@icloud.com>
2021-03-17 18:46:51 +0000

    v5.0.6 debug patch 1
    put troubleshooting console prints in ban command to locate where the error is.

--- commands/ban.js
+++ commands/ban.js
@@ -4,12 +4,19 @@ const mongoose = require("mongoose");
 
 module.exports.run = async (bot, message, args) => {
     await message.delete();
+    console.log("test1 pass");
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    console.log("test2 pass");
     if (!bUser) return message.reply("The specified user could not be found.");
+    console.log("test3 pass");
     let bReason = args.join(" ").slice(22);
+    console.log("test4 pass");
     if (!bReason) return message.reply("Please provide a reason");
+    console.log("test5 pass");
     if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You have not got the right permissions.");
+    console.log("test6 pass");
     if(bUser.hasPermission("MANAGE_MEMBERS")) return message.reply("That user cannot be banned.");
+    console.log("test7 pass");
 
     let banEmbed = new Discord.MessageEmbed()
         .setTitle("Ban")
@@ -18,8 +25,11 @@ module.exports.run = async (bot, message, args) => {
         .addField("Banned User", `${bUser} with the id ${bUser.id}`)
         .addField("Time", message.createdAt)
         .addField("Reason", bReason);
+    console.log("test8 pass");
     await message.guild.member(bUser).send(`You have been banned from ${message.guild.name} for ${bReason}`).catch(err => console.log(err));
+    console.log("test9 pass");
     message.guild.member(bUser).ban(bReason).then(message.channel.send(banEmbed)).catch(err => console.log(err));
+    console.log("test10 pass");
 
     const incident = new Incident({
       _id: mongoose.Types.ObjectId(),
@@ -33,8 +43,10 @@ module.exports.run = async (bot, message, args) => {
       iID: message.author.id,
       time: message.createdAt
     });
+    console.log("test11 pass");
 
     incident.save().catch(err => console.log(err));
+    console.log("test12  pass");
 }
 
 module.exports.help = {
