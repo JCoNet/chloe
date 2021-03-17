0ed56857d23b39628296e0a96821c992a5fb229b
JCoDog <jayconnolly14@icloud.com>
2021-03-17 20:47:41 +0000

    v5.0.7
    Fixed ban and kick commands.

--- commands/ban.js
+++ commands/ban.js
@@ -4,20 +4,12 @@ const mongoose = require("mongoose");
 
 module.exports.run = async (bot, message, args) => {
     await message.delete();
-    console.log("test1 pass");
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
-    console.log(`bUser returns: ${bUser}`);
-    console.log("test2 pass");
     if (!bUser) return message.reply("The specified user could not be found.");
-    console.log("test3 pass");
     let bReason = args.join(" ").slice(22);
-    console.log("test4 pass");
     if (!bReason) return message.reply("Please provide a reason");
-    console.log("test5 pass");
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("You have not got the right permissions.");
-    console.log("test6 pass");
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.reply("That user cannot be banned.");
-    console.log("test7 pass");
 
     let banEmbed = new Discord.MessageEmbed()
         .setTitle("Ban")
@@ -26,11 +18,8 @@ module.exports.run = async (bot, message, args) => {
         .addField("Banned User", `${bUser} with the id ${bUser.id}`)
         .addField("Time", message.createdAt)
         .addField("Reason", bReason);
-    console.log("test8 pass");
-    await message.guild.member(bUser).send(`You have been banned from ${message.guild.name} for ${bReason}`).catch(err => console.log(err));
-    console.log("test9 pass");
-    message.guild.member(bUser).ban(bReason).then(message.channel.send(banEmbed)).catch(err => console.log(err));
-    console.log("test10 pass");
+    await bUser.send(`You have been banned from ${message.guild.name} for ${bReason}`).catch(err => console.log(err));
+    await bUser.ban({reason: bReason}).then(message.channel.send(banEmbed)).catch(err => console.log(err));
 
     const incident = new Incident({
       _id: mongoose.Types.ObjectId(),
@@ -44,11 +33,8 @@ module.exports.run = async (bot, message, args) => {
       iID: message.author.id,
       time: message.createdAt
     });
-    console.log("test11 pass");
 
-    incident.save().catch(err => console.log(err));
-    console.log("test12  pass");
-}
+    await incident.save().catch(err => console.log(err));
 
 module.exports.help = {
   name: "ban"
--- commands/kick.js
+++ commands/kick.js
@@ -4,11 +4,11 @@ const mongoose = require("mongoose");
 
 module.exports.run = async (bot, message, args) => {
   await message.delete();
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
     if (!kUser) return message.reply("The specified user could not be found.");
     let kReason = args.join(" ").slice(22);
     if (!kReason) return message.reply("Please provide a reason");
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You have not got the right permissions.");
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("You have not got the right permissions.");
     if(kUser.hasPermission("MANAGE_MESSAGES")) return message.reply("That user cannot be kicked.");
 
     let kickEmbed = new Discord.MessageEmbed()
@@ -18,8 +18,8 @@ module.exports.run = async (bot, message, args) => {
         .addField("Kicked User", `${kUser} with the id ${kUser.id}`)
         .addField("Time", message.createdAt)
         .addField("Reason", kReason);
-    await message.guild.member(kUser).send(`You have been kicked from ${message.guild.name} for ${kReason}`).catch(err => console.log(err));
-    message.guild.member(kUser).kick(kReason).then(message.channel.send(kickEmbed)).catch(err => console.log(err));
+    await kUser.send(`You have been kicked from ${message.guild.name} for ${kReason}`).catch(err => console.log(err));
+    await kUser.kick(kReason).then(message.channel.send(kickEmbed)).catch(err => console.log(err));
 
     const incident = new Incident({
       _id: mongoose.Types.ObjectId(),
@@ -34,7 +34,7 @@ module.exports.run = async (bot, message, args) => {
       time: message.createdAt
     });
 
-    incident.save().catch(err => console.log(err));
+    await incident.save().catch(err => console.log(err));
 }
 
 module.exports.help = {
