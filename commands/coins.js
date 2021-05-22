60db0d02c41f15f38ce210bd846b586a174ed1b9
JCoDog <jayconnolly14@icloud.com>
2021-05-22 21:05:32 +0100

    testing mysql2
    test 31

--- commands/coins.js
+++ commands/coins.js
@@ -2,24 +2,25 @@ const Discord = require('discord.js');
 const mysql = require("mysql2/promise");
 
 module.exports.run = async (bot, message, args, connection) => {
-    console.log(connection);
     await message.delete();
 
     let result = await connection.query(`SELECT coins FROM money WHERE guildID = '${message.guild.id}' AND userID = '${message.author.id}'`);
-    // let results = result[0];
-    let embed = new Discord.MessageEmbed()
-        .setTitle("Coins")
-        .setColor("#aa7ce2")
-        .setDescription(`Coin Balance for ${message.author.username} on ${message.guild.name}.`)
-        .setThumbnail(message.author.displayAvatarURL);
-    if(!result[0]) {
-        embed.addField("Total", 0, true);
-        return message.channel.send(embed);
-    } else {
-        embed.addField("Coins", result[0].coins, true);
-        return message.channel.send(embed);
-    };
-}
+    console.log(`result: ${result[0]}`);
+    let results = result[0];
+    console.log(`results: ${results[0]}`);
+//     let embed = new Discord.MessageEmbed()
+//         .setTitle("Coins")
+//         .setColor("#aa7ce2")
+//         .setDescription(`Coin Balance for ${message.author.username} on ${message.guild.name}.`)
+//         .setThumbnail(message.author.displayAvatarURL);
+//     if(!result[0]) {
+//         embed.addField("Total", 0, true);
+//         return message.channel.send(embed);
+//     } else {
+//         embed.addField("Coins", result[0].coins, true);
+//         return message.channel.send(embed);
+//     };
+// }
 
 module.exports.help = {
     name: "coins"
