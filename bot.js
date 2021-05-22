46a6c069040be707a3de0620c75f4f34293d5dfe
JCoDog <jayconnolly14@icloud.com>
2021-05-22 17:43:33 +0100

    testing mysql2
    test 11

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 const fs = require("fs");
 const bot = new Discord.Client();
 const connection = mysql.createPool({
