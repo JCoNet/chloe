c821626cdf8ad1b19f8c98e46cfa42fa3cefebed
JCoDog <jayconnolly14@icloud.com>
2021-05-22 21:15:51 +0100

    testing mysql2
    test 32

--- commands/coins.js
+++ commands/coins.js
@@ -5,9 +5,9 @@ module.exports.run = async (bot, message, args, connection) => {
     await message.delete();
 
     let result = await connection.query(`SELECT coins FROM money WHERE guildID = '${message.guild.id}' AND userID = '${message.author.id}'`);
-    console.log(`result: ${result[0]}`);
+    console.log(`result: ${result}`);
     let results = result[0];
-    console.log(`results: ${results[0]}`);
+    console.log(`results: ${results}`);
 //     let embed = new Discord.MessageEmbed()
 //         .setTitle("Coins")
 //         .setColor("#aa7ce2")
