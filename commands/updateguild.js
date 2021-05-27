ebf345eb1c0405f830f137ff69f89423e39a5f4a
JCoDog <jayconnolly14@icloud.com>
2021-05-27 21:14:18 +0100

    v6.1.3
    fixed an issue where system channels can be vacant from a server causing a fatal error in chloe

--- commands/updateguild.js
+++ commands/updateguild.js
@@ -5,7 +5,17 @@ module.exports.run = async (bot, message, args, connection) => {
   await message.delete();
   if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("This command due to it's nature can only be run by a guild admin.");
   let defaultChannel = message.guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(message.guild.me).has('SEND_MESSAGES'));
-  await connection.query(`INSERT INTO guildConfig SET guildName = "${message.guild.name}", guildID = "${message.guild.id}", prefix = "chloe/", ownerName = "${message.guild.owner.user.username}", ownerID = "${message.guild.owner.user.id}", systemChannelName = "${message.guild.systemChannel.name}", systemChannelID = "${message.guild.systemChannel.id}", announcementChannelName = "${defaultChannel.name}", announcementChannelID = "${defaultChannel.id}", welcomeChannelName = "${defaultChannel.name}", welcomeChannelID = "${defaultChannel.id}", welcomeMessage = "Welcome to the server!"`).catch(err => console.log(err));
+  let sysChannel = message.guild.sysChannel;
+  let sysChannelName;
+  let sysChannelID;
+  if {sysChannel.length == 0} {
+    sysChannelName = defaultChannel.name;
+    sysChannelID = defaultChannel.id;
+  } else {
+    sysChannelName = message.guild.sysChannel.name;
+    sysChannelID = message.guild.sysChannel.id;
+  };
+  await connection.query(`INSERT INTO guildConfig SET guildName = "${message.guild.name}", guildID = "${message.guild.id}", prefix = "chloe/", ownerName = "${message.guild.owner.user.username}", ownerID = "${message.guild.owner.user.id}", systemChannelName = "${sysChannelName}", systemChannelID = "${sysChannelID}", announcementChannelName = "${defaultChannel.name}", announcementChannelID = "${defaultChannel.id}", welcomeChannelName = "${defaultChannel.name}", welcomeChannelID = "${defaultChannel.id}", welcomeMessage = "Welcome to the server!"`).catch(err => console.log(err));
   message.channel.send("Guild updated and now utilising new features.");
 };
 
