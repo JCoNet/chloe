37302ce60a15ee2465bde94e9e4e256c59edb9fb
JCoDog <jayconnolly14@icloud.com>
2021-05-27 20:32:21 +0100

    v6.1.2 test 3
    added .cache before .find

--- commands/updateguild.js
+++ commands/updateguild.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 
 module.exports.run = async (bot, message, args, connection) => {
   await message.delete();
-  let defaultChannel = await message.guild.channels.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'));
+  let defaultChannel = await message.guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'));
   await connection.query(`INSERT INTO guildConfig SET guildName = "${message.guild.name}", guildID = "${message.guild.id}", prefix = "chloe/", ownerName = "${message.guild.owner.username}", ownerID = "${message.guild.ownerID}", systemChannelName = "${message.guild.systemChannel}", systemChannelID = "${message.guild.systemChannelID}", announcementChannelName = "${defaultChannel.name}", announcementChannelID = "${defaultChannel.id}", welcomeChannelName = "${defaultChannel.name}", welcomeChannelID = "${defaultChannel.id}", welcomeMessage = "Welcome to the server!"`).catch(err => console.log(err));
   await message.channel.send("Guild updated and now utilising new features.");
 };
