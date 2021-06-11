54b64a346a74553ed384787c864a6433da940cfa
JCoDog <jayconnolly14@icloud.com>
2021-06-11 16:37:26 +0100

    v6.8.1

--- commands/stats.js
+++ commands/stats.js
@@ -9,19 +9,21 @@ module.exports.run = async (bot, message, args, connection, useprefix) => {
     .setDescription(stats.description)
     .setColor("#33fede")
     .setThumbnail(bicon)
-    .addField("Name", bot.user.username, true)
-    .addField("Created on", bot.user.createdAt, true)
-    .addField("Current version", stats.version, true)
-    .addField("Running On", `JCoNet BotFramework Version: ${stats.frmwrk}`, true)
-    .addField("Guild count", bot.guilds.cache.size, true)
-    .addField("This guild prefix", useprefix, true)
-    .addField("Save on online retail and support JCoNet!", '[Get Honey](https://www.joinhoney.com/ref/k4lomc4 "Get the Honey Extension and start saving on all your online purchases.")', true)
-    .addField("Support JCoNet another way!", '[Donate Here](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=78GGH2NZ57WUJ&source=url "Donate to JCoNet via PayPal on the JCoNet official website.")', true)
-    .addField("Affiliated with Streamlabs OBS", '[Get Streamlabs OBS Today](https://streamlabs.com/slobs/d/1789520 "Get Streamlabs OBS via our link to get the best streaming software and support future JCoNet streams and development.")', true)
-    .addField("Want cool merch?", '[Check out our merch store today!](https://teespring.com/stores/jconet "Visit the JCoNet official merchandise store today to hook yourself up with our latest branded items.")', true)
-    // .addField("Contributions", "Information on the support (financially) for this project")
-    // .addField("Total contributions", "£20")
-    // .addField("Top contribution", "JCoDog - £20")
+    .addFields(
+      {name: "Name", value: bot.user.username, inline: true},
+      {name: "Created on", value: bot.user.createdAt, inline: true},
+      {name: "Current version", value: stats.version, inline: true},
+      {name: "Running On", value: `JCoNet BotFramework Version: ${stats.frmwrk}`, inline: true},
+      {name: '\u200B', value: '\u200B', inline: false},
+      {name: "Guild count", value: bot.guilds.cache.size, inline: true},
+      {name: "This guild prefix", value: useprefix, inline: true},
+      {name: '\u200B', value: '\u200B', inline: false},
+      {name: "Support Chloe and JCoNet", value: '\u200B', inline: false},
+      {name: "Save on online retail and support JCoNet!", value: '[Get Honey](https://www.joinhoney.com/ref/k4lomc4 "Get the Honey Extension and start saving on all your online purchases.")', inline: true},
+      {name: "Support JCoNet another way!", value: '[Donate Here](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=78GGH2NZ57WUJ&source=url "Donate to JCoNet via PayPal on the JCoNet official website.")', inline: true},
+      {name: "Affiliated with Streamlabs OBS", value: '[Get Streamlabs OBS Today](https://streamlabs.com/slobs/d/1789520 "Get Streamlabs OBS via our link to get the best streaming software and support future JCoNet streams and development.")', inline: true},
+      {name: "Want cool merch?", value: '[Check out our merch store today!](https://merch.jconet.xyz "Visit the JCoNet official merchandise store today to hook yourself up with our latest branded items.")', inline: true},
+    )
     .setFooter(`Lead developer: ${stats.author}`);
   message.channel.send(statsembed);
 }
