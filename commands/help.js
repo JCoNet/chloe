5b47edcb9b4008e6de342c7a6711e21fb1bdbd88
JCoDog <jayconnolly14@icloud.com>
2021-06-11 17:14:43 +0100

    v6.8.3

--- commands/help.js
+++ commands/help.js
@@ -8,22 +8,41 @@ module.exports.run = async (bot, message, args, connection, useprefix) => {
         .setTitle("Chloe help")
         .setColor("#ff0000")
         .setDescription("A list of all the commands and how to use them.")
-        // Regular user commands
-        .addField("----{Member Commands}----", "~~~~")
-        .addField("Coins", `${useprefix}coins - Displays your coin count.`)
-        .addField("Stats", `${useprefix}stats - General information about the bot and how to support it's development.`)
-        // Moderator commands
-        .addField("----{Staff Commands}----", "~~~~")
-        .addField("Clear", `${useprefix}clear <num to delete> - removes set number of messages from chat.`)
-        .addField("Mute", `${useprefix}mute @user <time> - Mutes a user for the chosen time.`)
-        .addField("Kick", `${useprefix}kick @user <reason> - Kicks the selected user from the server.`)
-        .addField("Ban", `${useprefix}ban @user <reason> - Bans the selected user from the server.`)
-        .addField("Ping All", `${useprefix}pingall <message> - Send the message with an everyone tag.`)
-        // Administrator commands
-        .addField("----{Admin Commands}----", "~~~~")
-        .addField("Prefix", `${useprefix}prefix <new prefix> - Sets the new prefix for the server.`)
-        .addField("Add Role", `${useprefix}addrole @user <role> - Gives the user the role you specified.`)
-        .addField("Remove Role", `${useprefix}removerole @user <role> - Removes the user from the role you specified.`);
+        .setThumbnail(bot.user.displayAvatarURL())
+        .setAuthor('JCoNet Development', 'https://jconet.xyz/resources/JCN.png', 'https://jconet.xyz')
+        .setFooter("New commands are developed and added frequently."),
+        .addFields(
+            {name: "User Commands", value: '\u200B', inline: false},
+            {name: `${useprefix}coins`, value: "Displays your coin balance for this server.", inline: true},
+            {name: `${useprefix}stats`, value: "General information about the bot and how to support it's development.", inline: true},
+            {name: `${useprefix}help`, value: "Displays a list of commands that you can use in the server for 1 minute.", inline: true},
+        )
+    if (message.member.hasPermission("MANAGE_MESSAGES")) {
+        helpEmbed.addFields(
+            {name: "Moderator Commands", value: '\u200B', inline:false},
+            {name: `${useprefix}clear <num to delete>`, value: "removes set number of messages from chat.", inline: true},
+            {name: `${useprefix}pingall <message>`, value: "Send the message as Chloe with an everyone tag.", inline: true},
+            {name: `${useprefix}say <message>`, value: "Send a message as Chloe.", inline: true},
+            {name: `${useprefix}warn @user <reason>`, value: "Warns the selected user in the server." inline: true},
+        )
+    }
+    if (message.member.hasPermission("KICK_MEMBERS")) {
+        helpEmbed.addField(`${useprefix}kick @user <reason>`, "Kicks the selected user from the server.", true)
+    };
+    if (message.member.hasPermission("BAN_MEMBERS")) {
+        helpEmbed.addField(`${useprefix}ban @user <reason>`, "Bans the selected user from the server.", true)
+    };
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+        helpEmbed.addFields(
+            {name: "Administrator Commands", value: "\u200B", inline: false},
+            {name: `${useprefix}updateguild`, value: "Updates our database about info regarding your server. [only run when bot tells you to]", inline: true},
+            {name: `${useprefix}set`, value: "Will start the configurator for message channels using the channel it is run in as the channel you want the message channel set to.", inline: true},
+            {name: `${useprefix}prefix <new prefix>`, value: "Sets the new prefix for the server", inline: true},
+            {name: `${useprefix}addrole @user <role>`, value: "Gives the user the role you specified. [currently disabled in all servers]", inline: true},
+            {name: `${useprefix}removerole @user <role>`, value: "Removes the user from the role you specified. [currently disabled in all servers]", inline: true},
+        )
+    };
+        
     message.channel.send(helpEmbed).catch(err => console.log(err));
 
 };
