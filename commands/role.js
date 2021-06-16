9423889c7a674ea75fd4ca0a01167cd96da8162d
JCoDog <jayconnolly14@icloud.com>
2021-06-16 04:28:42 +0100

    v6.10.6

--- commands/role.js
+++ commands/role.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args, connection, useprefix) => {
     await message.delete();
     if (!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("You do not have the required permissions to run this command.").then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
     let operation = args[0].toLowerCase();
-    let user = message.mentions.members.first() || message.guild.members.cache.find(args[1]);
-    let role = message.mentions.roles.first() || message.guild.roles.cache.find(args[2]);
+    let user = message.mentions.members.first() || message.guild.members.cache.get(args[1]) || message.guild.members.cache.find(user => user.id === args[1]);
+    let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[2]) || message.guild.roles.cache.find(r => r.name === args[2]);;
 
     // check the arguments and variables exist.
     if (!args[0]) return message.channel.send("Please ensure you specify the operation for this command. (add/remove)").then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
