3318a5d3f713b05121e9f53ed3337d6ae9db20db
JCoDog <jayconnolly14@icloud.com>
2021-06-16 05:21:05 +0100

    v6.10.17

--- commands/role.js
+++ commands/role.js
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args, connection, useprefix) => {
     let s2 = args[2].toLowerCase();
     console.log(`arg 1: ${args[1]} arg 2: ${args[2]}`);
     console.log(`s1: ${s1} s2: ${s2}`);
-    let user = message.mentions.members.first() || message.guild.members.cache.get(s1);
+    let user = message.mentions.members.first() || message.guild.members.cache.get(s1) || message.guild.members.cache.get(bot.users.cache.find(u => u.tag === s1).id);
     let role = message.mentions.roles.first() || message.guild.roles.cache.get(s2);
     console.log(`user: ${user} role: ${role}`);
 
@@ -21,17 +21,17 @@ module.exports.run = async (bot, message, args, connection, useprefix) => {
     if (!args[2]) return message.channel.send("Please ensure you specify a role either by name, ID or mention.").then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
     if (!role) return message.channel.send("I could not find the specified role in the server.").then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
 
-    if (operation.toLowerCase() != "add" || operation.toLowerCase() != "remove") return message.channel.send("Please only specify valid command operations. (add/remove)").then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
+    if (operation != "add" || operation != "remove") return message.channel.send("Please only specify valid command operations. (add/remove)").then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
 
     // check roles and permissions
     if (user.roles.highest.position >= message.member.roles.highest.position) return message.channel.send("The specified user is ranked the same or higher than you, therefore I cannot allow you to modify this user's roles.").then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
     if (message.members.roles.highest.position <= role.position) return message.channel.send("The specified role is ranked the same as or higher than your highest ranked role, therefore I cannot let you assign it to anyone.").then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
 
     // add/remove the role
-    if (operation.toLowerCase() == "add") {
+    if (operation == "add") {
         await user.roles.add(role.id).catch(err => console.error(err));
         message.channel.send(`You successfully added ${user} to the ${role} role!`).then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
-    } else if (operation.toLowerCase() == "remove") {
+    } else if (operation == "remove") {
         await user.roles.remove(role.id).catch(err => console.error(err));
         message.channel.send(`You successfully removed ${user} from the ${role} role!`).then(msg => msg.delete({timeout: 3000})).catch(err => console.error(err));
     } else {
