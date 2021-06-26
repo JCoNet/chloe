437aeff0edac5efa3f98337050c07d2266351a86
JCoDog <jayconnolly14@icloud.com>
2021-06-26 13:56:37 +0100

    v6.11.4

--- commands/mod/pingall.js
+++ commands/mod/pingall.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "pingall",
   description: "Send an @everyone ping as the bot!",
-  args: false,
+  args: true,
   aliases: ["announce", "broadcast"],
   async execute(Discord, bot, connection, message, args, useprefix) {
     if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("OOF. You do not appear to have access to this command, you need to be able to manage messages to do this!");
