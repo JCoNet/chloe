e97371b5024be5d2b0f081fb131547ced8759811
JCoDog <jayconnolly14@icloud.com>
2021-06-26 17:42:52 +0100

    v6.11.9

---
+++ commands/fun/yeet.js
@@ -0,0 +1,37 @@
+module.exports = {
+    name: "ban",
+    description: "Ban a user!",
+    args: true,
+    usage: "<user>",
+    async execute(Discord, bot, connection, message, args, useprefix) {
+        await message.delete();
+        let cost = 25;
+        let result = await connection.query(`SELECT coins FROM money WHERE guildID = "${message.guild.id}" AND userID = "${message.author.id}"`).catch(err => console.error(err));
+        let results = result[0];
+        if (results.length == 0) {
+            return message.reply(`You currently have no coins to use. This command costs ${cost} coins!`);
+        };
+
+        if (results[0].coins < cost) {
+            return message.reply(`You do not have enough coins to use this command. This command costs ${cost} coins!`);
+        };
+
+        let newbal = results[0].coins - cost;
+        await connection.query(`UPDATE money SET coins="${newbal}" WHERE guildID="${message.guild.id}" ANDS userID="${message.author.id}"`).catch(err => console.error(err));
+
+        let yeetee = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+        let distance = Math.floor(Math.random() * 1000) +10;
+
+        let msg = message.reply(`you are now yeeting <@!${yeetee.id}>! 🛫.....`);
+        let embed = new Discord.MessageEmbed()
+        .setTitle("Ya Yeet!")
+        .setDescription("Enjoy the trip!")
+        .setColor("#980098")
+        .addFields(
+            {name: "Thrower", value: message.author.username, inline: true},
+            {name: "Recipient", value: yeetee.user.username, inline: true},
+            {name: "Distance", value: `${distance}ft`, inline: true},
+        )
+        .setFooter(`This command cost ${message.author.username} ${cost} coins to use...`);
+    },
+};
