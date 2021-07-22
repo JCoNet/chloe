1bd48824de957fef3bb05ca0b5a678e1b716033a
JCoDog <jayconnolly14@icloud.com>
2021-07-22 04:01:53 +0100

    v6.13.13

--- bot.js
+++ bot.js
@@ -72,13 +72,13 @@ listener.listen();
 
 async function setVars() {
   const one = 'jconet';
-  const two =  await apiClient.helix.users.getUserByName(one);
-  const three =   await two.id;
-  const four =  await bot.channels.cache.get('673427499396628493');
+  const two = await apiClient.helix.users.getUserByName(one);
+  const three = await two.id;
+  const four = await bot.channels.cache.get('673427499396628493');
   // we need to track the previous status of the stream because there are other state changes than the live/offline switch
-  let five =  await apiClient.helix.streams.getStreamByUserId(three);
+  let five = await apiClient.helix.streams.getStreamByUserId(three);
 
-  return [three, four, five];
+  return {three, four, five};
 }
 
 const [userId, streamChannel, prevStream] = setVars();
