64be3300e09e830aa1dec9665a9f0a8e6d427bf4
JCoDog <jcodog@jconet.co.uk>
2021-07-23 18:15:14 +0100

    Create chloeserver.js

---
+++ chloeserver.js
@@ -0,0 +1,12 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("CHLOE HOST 1 IS ONLINE\n");
+}).listen(8000);
