ff097af13b6aad216e9efb544a0aeae2e843ba2e
JCoDog <jcodog@jconet.co.uk>
2021-07-24 09:41:08 +0100

    v6.13.33

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-require('dotenv').config({ path: './.env' });
+require('dotenv').config({ path: '.env' });
 
 const Discord = require("discord.js");
 const mysql = require("mysql2/promise");
 const fs = require("fs");
-const stats = require("./package.json");
-const config = require("./botconfig.json");
+const stats = require("package.json");
+const config = require("botconfig.json");
 const ascii = require("ascii-table");
 
 const bot = new Discord.Client();
