b5b7c745c84c3b60e01e22641d71fb40678d6403
JCoDog <jcodog@jconet.co.uk>
2021-07-29 23:36:35 +0100

    Update guilds.js

--- commands/jcn/guilds.js
+++ commands/jcn/guilds.js
@@ -9,14 +9,14 @@ module.exports = {
     async execute(Discord, bot, connection, message, args, useprefix) {
         var table = new AsciiTable3('Chloe Guilds')
         .setHeading('Server Name', 'Server ID')
-        .setWidths(30,30)
+        .setWidths([30,30])
         .setCellMargin(0)
 
         let guilds = [];
         bot.guilds.cache.each(g => guilds.push({ name: g.name, id: g.id }));
         var len = guilds.length;
         for (var i = 0; i < len; i++) {
-            table.addRow(guilds[i].name, guilds[i].id).setAlign(30, AsciiTable3.CENTER);
+            table.addRow(guilds[i].name, guilds[i].id);
         }
         table.setStyle('unicode-single');
         message.channel.send(table.toString());
