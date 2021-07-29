9a999ce697f093d982248f67b37cc41b285dbd79
JCoDog <jcodog@jconet.co.uk>
2021-07-29 22:06:14 +0100

    Update guilds.js

--- commands/jcn/guilds.js
+++ commands/jcn/guilds.js
@@ -6,7 +6,7 @@ module.exports = {
     description: "JCoNet only command!",
     args: false,
     async execute(Discord, bot, connection, message, args, useprefix) {
-        const Guilds = bot.guilds.cache.map(guild => guild.name);
-        message.channel.send(Guilds);
+        const Guilds = await bot.guilds.cache.map(guild => guild.name);
+        console.log(Guilds);
     },
 };
