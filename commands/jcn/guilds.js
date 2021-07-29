c8f8c5d9030390b639b7a97d86fa2baee17a705d
JCoDog <jcodog@jconet.co.uk>
2021-07-29 22:40:37 +0100

    Update guilds.js

--- commands/jcn/guilds.js
+++ commands/jcn/guilds.js
@@ -1,13 +1,20 @@
 const config = require("../../botconfig.json");
 const stats = require("../../package.json");
+const ascii = require("ascii-table");
 
 module.exports = {
     name: "guilds",
     description: "JCoNet only command!",
     args: false,
     async execute(Discord, bot, connection, message, args, useprefix) {
-        const Guilds = await bot.guilds.cache.map(guild => guild.name);
-        console.log(Guilds);
-        message.channel.send( JSON.stringify(Guilds))
+        table = new ascii().setHeading("Server Name", "Server ID");
+
+        let guilds = [];
+        bbot.guilds.cache.each(g => guilds.push({ name: g.name, id: g.id }));
+        var len = guilds.length;
+        for (var i = 0; i < len; i++) {
+            table.addRow(guilds[i].name, guilds[i].id);
+        }
+        message.channel.send(table);
     },
 };
