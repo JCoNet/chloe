c5363f7c8c507587b0768a060595bdbfd5ed0f52
JCoDog <jcodog@jconet.co.uk>
2021-08-01 18:25:10 +0100

    Update kick.js

--- commands/mod/kick.js
+++ commands/mod/kick.js
@@ -10,18 +10,19 @@ module.exports = {
     let kReason = args.join(" ").slice(22);
     if (!kReason) return message.channel.send(`<@!${message.author.id}>, Please provide a reason`);
     if(!message.member.permissions.has("KICK_MEMBERS")) return message.channel.send(`<@!${message.author.id}>, You have not got the right permissions.`);
+    console.log(kUser);
     if(kUser.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`<@!${message.author.id}>, That user cannot be kicked.`);
 
-    let kickEmbed = new Discord.MessageEmbed()
-        .setTitle("Kick")
-        .setDescription("The following kick took place")
-        .setColor("#e68a00")
-        .addField("Kicked User", `${kUser} with the id ${kUser.id}`)
-        .addField("Time", message.createdAt)
-        .addField("Reason", kReason);
-    await kUser.send(`You have been kicked from ${message.guild.name} for ${kReason}`).catch(err => console.error(err));
-    await kUser.kick(kReason).then(message.channel.send({embeds: [kickEmbed]})).catch(err => console.error(err));
+  //   let kickEmbed = new Discord.MessageEmbed()
+  //       .setTitle("Kick")
+  //       .setDescription("The following kick took place")
+  //       .setColor("#e68a00")
+  //       .addField("Kicked User", `${kUser} with the id ${kUser.id}`)
+  //       .addField("Time", message.createdAt)
+  //       .addField("Reason", kReason);
+  //   await kUser.send(`You have been kicked from ${message.guild.name} for ${kReason}`).catch(err => console.error(err));
+  //   await kUser.kick(kReason).then(message.channel.send({embeds: [kickEmbed]})).catch(err => console.error(err));
     
-  await connection.query(`INSERT INTO incidents SET serverID = "${message.guild.id}", serverName = "${message.guild.name}", userID = "${kUser.id}", userName = "${kUser.user.username}", type = "KICK", reason = "${kReason}", dateAndTime = "${message.createdAt}", staffID = "${message.author.id}", staffName = "${message.author.username}"`);
+  // await connection.query(`INSERT INTO incidents SET serverID = "${message.guild.id}", serverName = "${message.guild.name}", userID = "${kUser.id}", userName = "${kUser.user.username}", type = "KICK", reason = "${kReason}", dateAndTime = "${message.createdAt}", staffID = "${message.author.id}", staffName = "${message.author.username}"`);
   },
 };
