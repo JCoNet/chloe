82c8f2ac5bb43df35f09a1d5a69c16510091ccfe
JCoDog <jcodog@jconet.co.uk>
2021-08-01 15:16:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,24 @@ const stats = require("./package.json");
 const config = require("./botconfig.json");
 const ascii = require("ascii-table");
 
-const bot = new Discord.Client();
-Intents = Discord.Intents();
+const intents = Discord.Intents();
+const bot = new Discord.Client({ intents: [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MEMBERS,
+  Intents.FLAGS.GUILD_BANS,
+  Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+  Intents.FLAGS.GUILD_INTEGRATIONS,
+  Intents.FLAGS.GUILD_WEBHOOKS,
+  Intents.FLAGS.GUILD_INVITES,
+  Intents.FLAGS.GUILD_VOICE_STATES,
+  Intents.FLAGS.GUILD_PRESENCES,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  Intents.FLAGS.GUILD_MESSAGE_TYPING,
+  Intents.FLAGS.DIRECT_MESSAGES,
+  Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+  Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+]});
 
 require("discord-buttons")(bot);
 const { MessageButton, MessageActionRow } = require("discord-buttons");
