5aa87841c989f21ab6992c69a22b691a3af8b21f
JCoDog <jcodog@jconet.co.uk>
2021-08-04 00:54:14 +0100

    Update threads.js

--- commands/support/threads.js
+++ commands/support/threads.js
@@ -9,21 +9,19 @@ module.exports = {
             let threadName = "New Thread!";
             let threadDescription = "A new thread to chat in!";
 
-            await message.reply("What is the desired name of the thread?").then(() => {
-                message.channel.awaitMessages(m => m.author.id == message.author.id,
-                    { max: 1, time: 30000 }).then(async collected => {
-                        threadName = collected.first().content;
-                        await message.reply("What is the desired description of the thread?").then(() => {
-                            message.channel.awaitMessages(m => m.author.id == message.author.id,
-                                { max: 1, time: 30000 }).then(collected => {
-                                    threadDescription = collected.first().content;
-                                }).catch(() => {
-                                    message.reply('No answer after 30 seconds, operation canceled.');
-                                });
-                        });    
+            await message.reply("What is the desired name of the thread?")
+            await message.channel.awaitMessages(m => m.author.id == message.author.id,
+            { max: 1, time: 30000 }).then(async collected => {
+                    threadName = collected.first().content;
+                    await message.reply("What is the desired description of the thread?");
+                    await message.channel.awaitMessages(m => m.author.id == message.author.id,
+                    { max: 1, time: 30000 }).then(collected => {
+                        threadDescription = collected.first().content;
                     }).catch(() => {
                         message.reply('No answer after 30 seconds, operation canceled.');
                     });
+            }).catch(() => {
+                message.reply('No answer after 30 seconds, operation canceled.');
             });
 
             message.channel.threads.create({
