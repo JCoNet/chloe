c59b5bcaa84b4f3415dffcf4d1b24a407838ede0
JCoDog <jcodog@jconet.co.uk>
2021-08-03 23:38:30 +0100

    v6.18.4

--- commands/support/threads.js
+++ commands/support/threads.js
@@ -2,25 +2,15 @@ module.exports = {
     name: "threads",
     description: "Manage threads",
     args: true,
-    usage: "<add/remove> <name> [add only: <private/public> <reason -optional>]",
+    usage: "<add/remove> <name> [add only: <reason -optional>]",
     async execute(Discord, bot, connection, message, args, useprefix) {
         if(args[0].toLowerCase()=="add") {
             // create thread
             let threadName = args[1];
-            let threadCategory = args[2];
-            let threadType = "";
             let threadDescription = "";
 
-            if (threadCategory == "private") {
-                threadType = "GUILD_PRIVATE_THREAD";
-            } else if (threadCategory == "public") {
-                threadType = "GUILD_PUBLIC_THREAD";
-            } else {
-                message.reply("Please specify a valid category type (public/private)");
-            };
-
-            if(args[3]) {
-                threadDescription = args[3];
+            if(args[2]) {
+                threadDescription = args[2];
             } else {
                 threadDescription = "A new thread!";
             };
@@ -28,8 +18,10 @@ module.exports = {
             message.channel.threads.create({
                 name: threadName,
                 autoArchiveDuration: 60,
-                type: threadType,
                 reason: threadDescription,
+            }).add({
+                member: message.author,
+                reason: "Created the thread request",
             }).then(threadChannel => threadChannel.send(threadDescription)).catch(err => console.error(err.message));
         };
     },
