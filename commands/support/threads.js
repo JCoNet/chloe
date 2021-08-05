6e90a5e5d27f2521879516e8ed74102cc31d870a
JCoDog <jcodog@jconet.co.uk>
2021-08-05 01:03:04 +0100

    vc6.18.6

--- commands/support/threads.js
+++ commands/support/threads.js
@@ -2,22 +2,22 @@ module.exports = {
     name: "threads",
     description: "Manage threads",
     args: true,
-    usage: "<add/remove> <name> [add only: <reason -optional>]",
+    usage: "<add/remove>",
     async execute(Discord, bot, connection, message, args, useprefix) {
         if(args[0].toLowerCase()=="add") {
             // create thread
             let threadName = "New Thread!";
             let threadDescription = "A new thread to chat in!";
 
-            await message.reply("What is the desired name of the thread?")
-            threadName = await message.channel.awaitMessages(m => m.author.id == message.author.id,{ max: 1, time: 30000 }).catch(message.reply("No response was said in time."));
-            await message.reply("What is the desired description of the thread?")
-            threadDescription = await message.channel.awaitMessages(m => m.author.id == message.author.id,{ max: 1, time: 30000 }).catch(message.reply("No response was said in time."));
+            await message.reply("What is the desired name of the thread?");
+            threadName = await message.channel.awaitMessages(m => m.author.id == message.author.id,{ time: 30000, max: 1, errors: ['time'] }).catch(message.reply("No response was said in time."));
+            await message.reply("What is the desired description of the thread?");
+            threadDescription = await message.channel.awaitMessages(m => m.author.id == message.author.id,{ time: 30000, max: 1, errors: ['time'] }).catch(message.reply("No response was said in time."));
 
             message.channel.threads.create({
                 name: threadName,
                 autoArchiveDuration: 60,
-                reason: threadDescription,
+                reason: `${message.author.user.username} requested the channel be created.`,
             })
             .then(threadChannel => threadChannel.send(threadDescription))
             .then(threadChannel => console.log(`Thread made: ${threadChannel.id}`))
