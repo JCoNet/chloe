503965e742a69a8c74d88973e15f9b2293e7a52c
JCoDog <jcodog@jconet.co.uk>
2021-08-05 02:42:57 +0100

    v6.18.14

--- commands/support/threads.js
+++ commands/support/threads.js
@@ -13,28 +13,30 @@ module.exports = {
             constructorMessages.push({id: message.channel.id});
 
             let sent1 = await message.reply("What is the desired name of the thread?");
-            constructorMessage.push({id: sent1.id});
+            constructorMessages.push({id: sent1.id});
             try {
                 let reply1 = await message.channel.awaitMessages({filter, time: 30000, max: 1, errors: ['time'] });
+                constructorMessages.push({id: reply1.first().id});
                 threadName = reply1.first().content;
                 console.log(threadName);
             } catch {
                 let error1 = message.reply("No response was said in time. Name not set. Command cancelled.");
-                constructorMessage.push({id: error1.id});
+                constructorMessages.push({id: error1.id});
             };
 
             let sent2 = await message.reply("What is the desired description of the thread?");
-            constructorMessage.push({id: sent1.id});
+            constructorMessages.push({id: sent1.id});
             try {
                 let reply2 = await message.channel.awaitMessages({filter, time: 30000, max: 1, errors: ['time'] });
+                constructorMessages.push({id: reply2.first().id});
                 threadDescription = reply2.first().content;
                 console.log(threadDescription);
             } catch {
                 let error2 = message.reply("No response was said in time. Description not set. Command cancelled.");
-                constructorMessage.push({id: error1.id});
+                constructorMessages.push({id: error1.id});
             };
 
-            var len = constructorMessage.length;
+            var len = constructorMessages.length;
             for (var i = 0; i < len; i++) {
                 let msg = await message.channel.fetchMessage(constructorMessage[i].id);
                 await msg.delete();
