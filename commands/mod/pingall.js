ed311e393f4379561797339c01cec3c813703d7c
JCoDog <jcodog@jconet.co.uk>
2021-08-05 05:29:49 +0100

    v6.18.23

--- commands/mod/pingall.js
+++ commands/mod/pingall.js
@@ -7,6 +7,9 @@ module.exports = {
     if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`OOF <@!${message.author.id}>. You do not appear to have access to this command, you need to be able to manage messages to do this!`);
     let botmessage = args.join(" ");
     await message.delete().catch(err => console.error(err));
-    message.channel.send(`@everyone ${botmessage}`);
+    let sent = message.channel.send(`@everyone ${botmessage}`).catch(err => console.log(err));
+    if (message.author.username === "JCoDog") {
+      sent.react("<:JCNVerifiedDeveloper:872672468765118544> ");
+    };
   },
 };
--- commands/mod/say.js
+++ commands/mod/say.js
@@ -7,6 +7,9 @@ module.exports = {
     if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send(`OOF <@!${message.author.id}>. You do not appear to have access to this command, you need to be able to manage messages to do this!`);
     let botmessage = args.join(" ");
     await message.delete().catch(err => console.error(err));
-    message.channel.send(botmessage).catch(err => console.error(err));
+    let sent = message.channel.send(botmessage).catch(err => console.error(err));
+    if (message.author.username === "JCoDog") {
+      sent.react("<:JCNVerifiedDeveloper:872672468765118544> ");
+    };
   },
 };
