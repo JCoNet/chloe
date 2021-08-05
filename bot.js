3715af740d5113123c2d22f17e2df827f07aa944
JCoDog <jcodog@jconet.co.uk>
2021-08-05 07:20:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -102,7 +102,7 @@ bot.on('guildCreate', async guild => {
     sysChannelID = guild.sysChannel.id;
   };
 
-  let guildOwner = await member.guild.fetchOwner();
+  let guildOwner = await guild.fetchOwner();
 
   let result = await connection.query(`SELECT * FROM guildConfig WHERE guildID="${guild.id}"`).catch(err => console.error(err));
   let results = result[0];
