eca73a8f9c739a9499d42dc1b0f3f39330cddc73
JCoDog <jcodog@jconet.co.uk>
2021-08-05 01:43:01 +0100

    v6.18.12
    This should be a working fix for the threads issue.

--- commands/support/threads.js
+++ commands/support/threads.js
@@ -6,16 +6,26 @@ module.exports = {
     async execute(Discord, bot, connection, message, args, useprefix) {
         if(args[0].toLowerCase()=="add") {
             // create thread
-            let threadName = "New Thread!";
-            let threadDescription = "A new thread to chat in!";
+            let threadName;
+            let threadDescription;
             let filter = m => m.author.id == message.author.id;
 
             await message.reply("What is the desired name of the thread?");
             try {
-                threadName = await message.channel.awaitMessages({filter, time: 30000, max: 1, errors: ['time'] });
+                let reply1 = await message.channel.awaitMessages({filter, time: 30000, max: 1, errors: ['time'] });
+                threadName = reply1.first().content;
                 console.log(threadName);
             } catch {
-                message.reply("No response was said in time.");
+                message.reply("No response was said in time. Name not set. Command cancelled.");
+            };
+
+            await message.reply("What is the desired description of the thread?");
+            try {
+                let reply2 = await message.channel.awaitMessages({filter, time: 30000, max: 1, errors: ['time'] });
+                threadDescription = reply2.first().content;
+                console.log(threadDescription);
+            } catch {
+                message.reply("No response was said in time. Description not set. Command cancelled.");
             };
             
             // await message.reply("What is the desired description of the thread?");
