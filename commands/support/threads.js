06bb1d81f39b4c0b06c1cdc81e98505067b9f609
JCoDog <jcodog@jconet.co.uk>
2021-08-05 01:38:42 +0100

    v6.18.11

--- commands/support/threads.js
+++ commands/support/threads.js
@@ -11,22 +11,27 @@ module.exports = {
             let filter = m => m.author.id == message.author.id;
 
             await message.reply("What is the desired name of the thread?");
-            await message.channel.awaitMessages({filter, time: 30000, max: 1, errors: ['time'] })
-            .then(collected => threadName = collected[0].content)
-            .catch(message.reply("No response was said in time. (30s)"));
-            console.log(threadName);
+            try {
+                threadName = await message.channel.awaitMessages({filter, time: 30000, max: 1, errors: ['time'] });
+                console.log(threadName);
+            } catch {
+                message.reply("No response was said in time.");
+            };
             
             // await message.reply("What is the desired description of the thread?");
             // threadDescription = await message.channel.awaitMessages(m => m.author.id == message.author.id,{ time: 30000, max: 1, errors: ['time'] }).catch(message.reply("No response was said in time."));
 
-            message.channel.threads.create({
-                name: threadName,
-                autoArchiveDuration: 60,
-                reason: `${message.author.username} requested the channel be created.`,
-            })
-            .then(threadChannel => threadChannel.send(threadDescription))
-            .then(threadChannel => console.log(`Thread made: ${threadChannel.id}`))
-            .catch(err => console.error(err.message));
+            try {
+                let threadChannel = await message.channel.threads.create({
+                    name: threadName,
+                    autoArchiveDuration: 60,
+                    reason: `${message.author.username} requested the channel be created.`,
+                });
+                threadChannel.send(threadDescription)
+                console.log(`Thread made: ${threadChannel.id}`)
+            } catch {
+                err => console.log(err);
+            };
         };
     },
 };
