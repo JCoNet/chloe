d8a74ba66c941064230556a5854d5b987fa37288
JCoDog <jcodog@jconet.co.uk>
2021-08-11 13:34:51 +0100

    v6.19.9

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -4,18 +4,16 @@ module.exports = {
     args: false,
     async execute(Discord, bot, connection, interaction) {
         let msg = await interaction.reply({content: "🏓 Pinging bot and api.....", ephemeral: true});
-        let latency = Math.floor(msg.createdTimestamp - interaction.timestamp);
         let apiLatency = Math.round(bot.ws.ping);
 
         let embed = new Discord.MessageEmbed()
         .setTitle("Bot ping")
         .setDescription("Latency of the server to the bot and the bot to the discord api")
         .addFields([
-            {name: "Bot Latency", value: `${latency}ms`, inline: true},
-            {name: "API Latency", value: `${apiLatency}ms`, inline: true},
+            {name: "Latency", value: `${apiLatency}ms`, inline: true},
         ])
         .setFooter("🏓 PONG!");
 
-        interaction.reply({embeds: [embed], ephemeral: true});
+        interaction.editReply({content: "🏓 Pinging bot and api.....", embeds: [embed], ephemeral: true});
     },
 };
