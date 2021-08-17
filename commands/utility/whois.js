b7ddfd4bfc3b689383e3353bc0dde7efca95d2d7
JCoDog <jcodog@jconet.co.uk>
2021-08-17 03:29:46 +0100

    Update whois.js

--- commands/utility/whois.js
+++ commands/utility/whois.js
@@ -20,15 +20,18 @@ module.exports = {
 
         // .toLocaleString('en-GB', { timeZone: 'Europe/London' })
         let now = new Date();
-        let joinDate = member.joinedAt;
-        let diff = now - joinDate;
-        let diffFormatted = formatDistance(now, joinDate, {includeSeconds: true})
+        let joined = member.joinedAt;
+        let diff = now - joined;
+        let diffFormatted = formatDistance(now, joined, {includeSeconds: true})
+
+        joinDate = joined.getDate();
+        joinTime = joined.getTime();
 
         console.log(now);
         console.log(joinDate);
         console.log(diff);
         console.log(diffFormatted);
 
-        interaction.reply({content: `<@!${user.id}> joined on the date: ${joinDate}! That was ${diffFormatted} ago!`});
+        interaction.reply({content: `<@!${user.id}> joined on the date: ${joinDate} and time: ${joinTime}! That was ${diffFormatted} ago!`});
     },
 };
