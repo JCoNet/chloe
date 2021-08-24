8150ab61e0f2fffc71514935804e4da420bf6faa
JCoDog <jcodog@jconet.co.uk>
2021-08-24 19:59:15 +0100

    v6.20.0

--- commands/jcn/registercommands.js
+++ commands/jcn/registercommands.js
@@ -3,52 +3,9 @@ module.exports = {
     description: "JCoNet only command!",
     args: false,
     async execute(Discord, bot, connection, message, args, useprefix) {
-        // bot.application?.commands.create({
-        //     name: 'ping',
-        //     description: 'Test the latency of this bot!'
-        // }).then(cmd => console.log(cmd));    
-        
-        // bot.application?.commands.create({
-        //     name: 'role',
-        //     description: 'Add or remove a role from a user.',
-        //     options: [
-        //         {
-        //             type: 'STRING',
-        //             name: 'operation',
-        //             description: 'What would you like to do?',
-        //             required: true,
-        //             choices: [{
-        //                 name: 'add',
-        //                 value: 'add'
-        //             }, {
-        //                 name: 'remove',
-        //                 value: 'remove'
-        //             }],
-        //         }, {
-        //             type: 'USER',
-        //             name: 'user',
-        //             description: 'The user mention or id to target.',
-        //             required: true,
-        //         }, {
-        //             type: 'ROLE',
-        //             name: 'role',
-        //             description: 'The role mention or id to select.',
-        //             required: true,
-        //         }
-        //     ],
-        // }).then(cmd => console.log(cmd));
-
         bot.application?.commands.create({
-            name: "whois",
-            description: "Lookup info about you or another user!",
-            options: [
-                {
-                    type: "USER",
-                    name: "user",
-                    description: "The user to target.",
-                    required: false,
-                },
-            ],
+            name: "id",
+            description: "View your very own JCoNet Digital ID!",
         }).then(cmd => console.log(cmd));
     },
 };
---
+++ commands/utility/id.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "id",
+    description: "View your very own JCoNet Digital ID!",
+    async execute(Discord, bot, connection, interaction) {
+        let result = await connection.query(`SELECT * FROM digitalID WHERE userID = "${interaction.user.id}"`).catch(err => console.error(err));
+        let results = result[0];
+
+        interaction.reply({ content: `${results}`, ephemeral: true});
+    },
+};
