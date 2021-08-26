c1ba3a9f6118571ed7acbc2befaf67b44b416f0b
JCoDog <jcodog@jconet.co.uk>
2021-08-26 16:35:33 +0100

    Update verify.js

--- commands/jcn-verification/verify.js
+++ commands/jcn-verification/verify.js
@@ -2,13 +2,14 @@ module.exports = {
     name: "verify",
     description: "JCoNet Verification API Command!",
     async execute(Discord, bot, connection, interaction) {
-        let result = connection.query(`SELECT integrationRoleID FROM guildConfig WHERE guildID = ${interaction.guild.id} AND integrationEnabled = 1`);
+        let result = connection.query(`SELECT integrationRoleID FROM guildConfig WHERE guildID = '${interaction.guild.id}' AND integrationEnabled = 1`);
         let results= result[0];
         if (result.length == 0) {
             return interaction.reply({content: "This guild does not have the Verification Integratiuon purchased. Please ask the owner to do this if you feel they need it.", ephemeral: true});
         }
 
-        console.log(results.toString());
+        console.log(result);
+        console.log(results);
 
         // let results = result[0];
         // let roleID = results[0].integrationRoleID;
