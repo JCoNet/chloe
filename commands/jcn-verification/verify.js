e8e561053871d8d7cc69f6afe9e5d218bf954eea
JCoDog <jcodog@jconet.co.uk>
2021-08-26 16:47:46 +0100

    Update verify.js

--- commands/jcn-verification/verify.js
+++ commands/jcn-verification/verify.js
@@ -9,6 +9,26 @@ module.exports = {
 
         // let results = result[0];
         let roleID = result[0][0].integrationRoleID;
+
+        let role = interaction.guild.roles.cache.find(r => r.id === roleID);
+
+        let result = await connection.query(`SELECT userVerified, ageVerified FROM digitalID WHERE userID = '${interaction.user.id}'`);
+        if (result[0].length == 0) {
+            return interaction.reply({content: "You do not have a JCN ID. Please obtain one at https://id.jconet.co.uk", ephemeral: true});
+        }
+
+        let verify = false;
+        
+        if (result[0][0].userVerified == 1 && result[0][0].ageVerified == 1) {
+            verify = true;
+        }
+
+        if (verify==true) {
+            await interaction.user.roles.add(role.id).catch(err => console.error(err));
+            return interaction.reply({content: `You have been verified and given the role ${role.name}`, ephemeral: true})
+        } else {
+            return interaction.reply({content: "You are not eligable to be verified. You must pass age and user verification. do /id to check where you failed.", ephemeral: true});
+        }
         interaction.reply({content: `${roleID}`, ephemeral: true});
     },
 };
