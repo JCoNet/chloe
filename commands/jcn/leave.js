7e320afd6d58e9f175a2949786a1f5c87c487d26
JCoDog <jcodog@jconet.co.uk>
2021-09-28 09:06:48 +0100

    Create leave.js

---
+++ commands/jcn/leave.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    name: "leave",
+    description: "JCoNet only command!",
+    args: false,
+    async execute(Discord, bot, connection, message, args, useprefix) {
+        await message.delete().catch(err => console.error(err));
+        if (message.author.id !== config.developer) return message.author.send("You are not allowed to use a JCoNet Developer only command.").catch(err => console.error(err));
+        
+        message.guild.leave();
+    }
+}
