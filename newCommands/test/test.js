c4868f8ad1e4c82529d85301a96b1785645c73b5
JCoDog <jcodog@jconet.co.uk>
2021-12-21 18:42:24 +0000

    Update test.js

--- newCommands/test/test.js
+++ newCommands/test/test.js
@@ -6,9 +6,12 @@ module.exports = {
         .setDescription("Test the bot!"),
 
     async execute (Discord, bot, connection, interaction) {
-        let msg = await interaction.reply({content: `Channel: ${interaction.channel.name}`});
-        interaction.channel.send({content: `\`${msg}\``})
-        msg = await interaction.editReply({content: msg + `\nCurrent guild: ${interaction.guild.name}`});
-        msg = await interaction.editReply({content: msg + `\nBot mode: ${process.env.ENV}`});
+        let msg = [];
+        msg.push(`Channel: ${interaction.channel.name}`);
+        await interaction.reply({content: msg, ephemeral: true});
+        msg.push(`\nCurrent guild: ${interaction.guild.name}`);
+        await interaction.editReply({content: msg, ephemeral: true});
+        msg.push(`\nBot mode: ${process.env.ENV}`);
+        await interaction.editReply({content: msg, ephemeral: true});
     }
 }
