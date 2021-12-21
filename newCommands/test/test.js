e5fe5cbe0fef2b22ddb8ca1063ff8989ab729516
JCoDog <jcodog@jconet.co.uk>
2021-12-21 18:34:49 +0000

    Update test.js

--- newCommands/test/test.js
+++ newCommands/test/test.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute (Discord, bot, connection, interaction) {
         const streamChannel = bot.channels.cache.get('818685046302965801');
         let msg = await interaction.reply({content: `Channel: ${JSON.stringify(streamChannel)}`, ephemeral: true});
-        interaction.channel.send({content: `\`${msg}\``})
+        interaction.channel.send({content: `\`${msg.content}\``})
         msg = await interaction.editReply({content: msg + `\nCurrent guild: ${interaction.guild.name}`, ephemeral: true});
         msg = await interaction.editReply({content: msg + `\nBot mode: ${process.env.ENV}`, ephemeral: true});
     }
