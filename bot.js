b5400b05312f8cf00adc64014081e06afa1a63f3
JCoDog <jcodog@jconet.co.uk>
2021-12-21 22:28:42 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,6 +137,39 @@ bot.once('ready', async () => {
   }).setToken(process.env.betatoken)
 
   try {
+    let result = await connection.query(`SELECT guildID, administratorRoleID FROM guildConfig WHERE guildID = ${process.env.testserver}`).catch(err => console.error(err));
+    let results = result[0];
+    var len = results.length;
+    for (var i = 0; i < (len); i++) {
+      await rest.put(Routes.applicationGuildCommands(botID, process.env.testserver), {
+        body: guildCommands
+      }).then(console.log("Guild Commands set"));
+
+      let guild = await bot.guilds.cache.get(process.env.testserver);
+
+      let permission1 = {
+        id: guild.roles.everyone.id,
+        type: 'ROLE',
+        permission: false,
+      };
+      let permission2 = {
+        id: results[i].administratorRoleID,
+        type: 'ROLE',
+        permission: true,
+      };
+
+      let commandsList = await guild.commands.fetch();
+      await commandsList.forEach(slashCommand => {
+        //set the permissions for each slashCommand
+        guild.commands.permissions.add({
+            command: slashCommand.id,
+            permissions: [permission1, permission2],
+        });
+      });
+      console.log("Guild command permissions set.")
+    };
+
+
     if (process.env.ENV === "production") {
       await rest.put(Routes.applicationCommands(botID), {
         body: globalCommands
@@ -176,35 +209,6 @@ bot.once('ready', async () => {
 
       console.log("Chloe has registered all commands.");
     } else {
-      let result = await connection.query(`SELECT guildID, administratorRoleID FROM guildConfig WHERE guildID = ${process.env.testserver}`).catch(err => console.error(err));
-      let results = result[0];
-      await rest.put(Routes.applicationGuildCommands(botID, process.env.testserver), {
-        body: guildCommands
-      }).then(console.log("Guild Commands set"));
-
-      let guild = await bot.guilds.cache.get(process.env.testserver);
-
-      let permission1 = {
-        id: guild.roles.everyone.id,
-        type: 'ROLE',
-        permission: false,
-      };
-      let permission2 = {
-        id: results[0].administratorRoleID,
-        type: 'ROLE',
-        permission: true,
-      };
-
-      let commandsList = await guild.commands.fetch();
-      await commandsList.forEach(slashCommand => {
-        //set the permissions for each slashCommand
-        guild.commands.permissions.add({
-            command: slashCommand.id,
-            permissions: [permission1, permission2],
-        });
-      });
-      console.log("Guild command permissions set.")
-
       await rest.put(Routes.applicationGuildCommands(botID, process.env.testserver), {
         body:  globalCommands
       }).then(console.log("Global Commands set"));
