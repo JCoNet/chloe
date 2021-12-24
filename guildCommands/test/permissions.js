912d08f842ac623f4643488008b013de455a7e80
JCoDog <jcodog@jconet.co.uk>
2021-12-24 20:24:58 +0000

    v6.31.6

---
+++ guildCommands/test/permissions.js
@@ -0,0 +1,58 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("permissions")
+        .setDescription("Check permissions of a user, role or channel")
+        .addSubcommand(subcommand => subcommand
+            .setName("channel")
+            .setDescription("Get permissions of a channel.")
+            .addChannelOption(option => option
+                .setName("target")
+                .setDescription("The channel to check")
+                .setRequired(true),
+            )
+            .setRequired(true),
+        )
+        .addSubcmmand(subcommand => subcommand
+            .setName("role")
+            .setDescription("Get permissions of a role")
+            .addRoleOption(option => option
+                .setName("target")
+                .setDescription("The role to check")
+                .setRequired(true),   
+            )
+            .setRequired(true),  
+        )
+        .addSubcommand(subcommand => subcommand
+            .setName("user")
+            .setDescription("Get permissions of a user")
+            .addUserOption(option => option
+                .setName("target")
+                .setDescription("The user to check")
+                .setRequired(true),    
+            )
+            .setRequired(true)
+        ),
+
+    async execute (interaction) {
+        // Code to run when executed.
+        let option = interaction.options.getSubcommand();
+
+        if (option.getName() === "channel") {
+            let target = interaction.options.getChannel("target");
+            let permissions = target.permissionOverwrites;
+            interaction.reply({content: `The permissions of the channel **${target.name}** are as follows:\n\`${permissions.toString()}\``})
+        } else if (option.getName() === "user") {
+            let target = interaction.options.getUser("target");
+            let permissions = channel.permissions;
+            interaction.reply({content: `The permissions of the user **${target.username}** are as follows:\n\`${permissions.toString()}\``})
+        } else if (option.getName() === "role") {
+            let target = interaction.options.getRole("target");
+            let permissions = channel.permissions;
+            interaction.reply({content: `The permissions of the role **${target.name}** are as follows:\n\`${permissions.toString()}\``})
+        } else {
+            interaction.reply({content: "There was no selected operation to perform.", ephemeral: true});
+        }
+    }
+};
--- guildCommands/test/verify.js
+++ guildCommands/test/verify.js
@@ -35,7 +35,7 @@ module.exports = {
             await interaction.member.roles.add(role.id).catch(err => console.error(err));
             return interaction.reply({content: `You have been verified and given the role: **${role.name}**`, ephemeral: true});
         } else {
-            return interaction.reply({content: "You are not eligable to be verified. **You must pass age and user verification**. do /id to check where you failed.", ephemeral: true});
+            return interaction.reply({content: "You are not eligable to be verified. **You must pass age and user verification**. do /id to check where you failed. To verify please do so at https://id.jconet.co.uk", ephemeral: true});
         }
     }
 };
